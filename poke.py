a1cfaf0343a52fd8fe05a828e23447b25d39372e
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2018-07-19 11:05:42 +0900

    first commit

---
+++ poke.py
@@ -0,0 +1,13 @@
+class Pika():
+    def __init__(self):
+        self.name="피카츄"
+        self.hp=300
+        self.type="전기"
+        self.acu=50
+    def skill_a(self):
+        self.skill_a_name="10만볼트"
+        self.skill_a_damage=100
+    def skill_b(self):
+        self.skill_b_name="몸통박치기"
+        self.skill_a_damage = 50
+
