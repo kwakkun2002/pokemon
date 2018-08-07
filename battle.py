a1532576c08bdf7dc8f03b63f0b757270826e18e
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2018-08-08 00:08:25 +0900

    8/8 곽건:poke.py는 현재 단계 완성
    이제 map이랑 battle을 만들어야 함
    player도

--- battle.py
+++ battle.py
@@ -1,31 +1,7 @@
-# import pygame as pg
-# pg.init()
-#
-# display_width=800
-# display_height=600
-#
-# ourScreen=pg.display.set_mode((display_width,display_height))
-# pg.display.set_caption('파이게임')
-# myImg=pg.image.load("coil.png")
-#
-# def myimg(x,y):
-#     ourScreen.blit(myImg,(x,y))
-#
-# x=(display_height*0.5)
-# y=(display_width*0.5)
-#
-# finish=False
-# while not finish:
-#     for event in pg.event.get():
-#         if event.type==pg.QUIT:
-#             finish=True
-#     ourScreen.fill((255,255,255))
-#     myimg(x,y)
-#     pg.display.flip()
-# pg.quit()
-# quit()
 
-import poke as pk
+
+
+
 
 
 
